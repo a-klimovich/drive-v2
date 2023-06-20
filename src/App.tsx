22af2f6c28568ed061efd7bb3c8ec5e061e43b6d
a-klimovich <ar2r.klimovich@gmail.com>
2023-06-20 14:41:17 +0200

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ const App = () => {
   return (
     <>
       <h1>Vite + React</h1>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import svgr from "vite-plugin-svgr";
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr({
-    exportAsDefault: true,
-  })],
+  plugins: [
+    react(),
+    svgr({
+      exportAsDefault: true,
+    }),
+  ],
 })
