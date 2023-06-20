108138b3fa2970d86693d6f071f3bdca24d9f601
tomas-pasta <tscik228@gmail.com>
2023-06-20 13:02:32 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+    </>
+  )
+}
+
+export default App
